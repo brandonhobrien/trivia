081bb2c9cecff46f322fd912bbb2ea93ab697408
Brandon O'Brien <31778561+brandonhobrien@users.noreply.github.com>
2020-11-24 14:21:57 -0800

    Progress working with top NAV bar

--- trivia-app/src/App.vue
+++ trivia-app/src/App.vue
@@ -1,18 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <nav>
+      <div>
+        <router-link to="/">Trivia Home</router-link> |
+        <router-link to="/Dogs">Dogs</router-link> |
+        <router-link to="/UserScores">User Scores</router-link>
+      </div>
+      <div class="navigation__user">
+        User: {{ fullName }}
+      </div>
+    </nav>
+    <b-container>
+      <router-view/>
+    </b-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      user: {
+        id: 1,
+        username: '_bobrien211',
+        firstName: 'Brandon',
+        lastName: 'OBrien',
+        email: 'brandonhobrien@gmail.com',
+        idAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+
   }
 }
 </script>
+
 
 <style>
 #app {
@@ -21,6 +51,32 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 5%;
+    background-color: rgb(10, 200, 180, 0.8);
+    color: white;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+
+}
+
+.navigation__logo {
+      font-size: 24px;
+}
+
+.navigation__user {
+}
+
 </style>
