742cf27fe3bfadc442f1800fd4089105ca20c1f4
Brandon O'Brien <31778561+brandonhobrien@users.noreply.github.com>
2020-11-24 16:04:13 -0800

    Bootstrap isnt linking propperly. done for day

--- trivia_app/src/main.js
+++ trivia_app/src/main.js
@@ -1,7 +1,9 @@
+import './plugins/bootstrap-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(BootstrapVue).mount('#app')
